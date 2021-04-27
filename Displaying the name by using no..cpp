7243c8eadb992cf5e3788e1f3a7d18e3c1016052
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2021-04-27 09:05:29 +0530

    To display the name by using number.

---
+++ Displaying the name by using no..cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+	int a;
+	cout<<"enter a number: \n";
+	cin>>a;
+	
+	a>9 && a<=99? cout<<"my name is rahul sharma.": cout<<"your number is: \n";
+	return 0;
+}
